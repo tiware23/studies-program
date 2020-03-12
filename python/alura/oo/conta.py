efc6c12c50e0ebbca770442bc2357a0d95626648
Thiago do Nascimento Cavalcante <tiware@Thiagos-MacBook-Air.local>
2020-03-11 22:57:55 -0300

    Private attr

--- python/alura/oo/conta.py
+++ python/alura/oo/conta.py
@@ -1,17 +1,17 @@
 class Conta:
     def __init__(self,numero, titular, saldo, limite=1000.0):
         print("Construindo objeto...{}".format(self))
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
 
     def extrato(self):
-        print("Saldo de {} do titular {}".format(self.saldo, self.titular))
+        print("Saldo de {} do titular {}".format(self.__saldo, self.__titular))
     
     def deposita(self, valor):
-        self.saldo += valor
+        self.__saldo += valor
     
     def saca(self, valor):
-        self.saldo -= valor
+        self.__saldo -= valor
         
