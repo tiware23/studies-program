6ff3604d05d32fc215ac966b6b14356c578a978a
Thiago Cavalcante <thiago.cavalcante@C02PHD6TG3QC.local>
2020-03-22 14:53:07 -0300

    Remember class

---
+++ python/alura/oo2/modelo.py
@@ -0,0 +1,54 @@
+class Filme:
+    def __init__(self, nome, ano, duracao):
+        self.__nome = nome.title()
+        self.ano = ano
+        self.duracao = duracao
+        self.__likes = 0
+
+    @property
+    def likes(self):
+        return self.__likes
+
+    def dar_like(self):
+        self.__likes += 1
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self.__nome = novo_nome.title()
+
+class Serie:
+    def __init__(self, nome, ano, temporadas):
+        self.__nome = nome.title()
+        self.ano = ano
+        self.temporadas = temporadas
+        self.__likes = 0
+
+    @property
+    def likes(self):
+        return self.__likes
+
+    def dar_like(self):
+        self.__likes += 1
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self.__nome = novo_nome.title()
+
+
+vingadores = Filme('vingadaores - guerra infinita', 2018, 160)
+vingadores.dar_like()
+vingadores.dar_like()
+
+print(f'Nome: {vingadores.nome} - Ano: {vingadores.ano}' f' - Duracao: {vingadores.duracao} - Likes: {vingadores.likes}')
+
+atlanta = Serie('atlanta', 2018, 2)
+atlanta.dar_like()
+print(f'Nome: {atlanta.nome} - Ano: {atlanta.ano}' f' - Temporada: {atlanta.temporadas} - Likes: {atlanta.likes}')
