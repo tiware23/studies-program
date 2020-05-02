3d5aa12e5661592a54b44d397f13bfac8193af75
Thiago Cavalcante <thiagocavalcante@Thiagos-MacBook-Air.local>
2020-05-02 12:01:27 -0300

    Implement log

--- python/headFirstPython/chapter_5/webapp/vsearch4web.py
+++ python/headFirstPython/chapter_5/webapp/vsearch4web.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request,  escape
 from vsearch import search4letters
 
 app = Flask(__name__)
+
+def log_request(req: 'flask_request', res: str) -> None:
+    with open('vsearch.log', 'a') as log:
+        print(req.form, req.remote_addr, req.user_agent, res, file=log, sep='|')
 
 @app.route('/search4', methods=['POST']) # GET is Default Method.
 def do_search() -> 'html':
@@ -10,6 +14,7 @@ def do_search() -> 'html':
     letters = request.form['letters']
     title = 'Here are your results:'
     results = str(search4letters(phrase, letters))
+    log_request(request, results)
     return render_template('results.html', the_title=title, the_results=results, 
                            the_letters=letters, the_phrase=phrase,)
 
@@ -17,6 +22,17 @@ def do_search() -> 'html':
 @app.route('/entry')
 def entry_page() -> 'html':
     return render_template('entry.html', the_title="Welcome to search4letters on the web!")
+
+@app.route('/viewlog')
+def view_the_log() -> str:
+    contents = []
+    with open('vsearch.log') as log:
+        for line in log:
+            contents.append([])
+            for item in line.split("|"):
+                contents[-1].append(escape(item))
+        
+    return str(contents)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
