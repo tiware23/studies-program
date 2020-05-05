1c74db5417734c22f04781668c4b5919eb3eebdc
Thiago Cavalcante <thiagocavalcante@Thiagos-MacBook-Air.local>
2020-05-05 11:29:00 -0300

    Implement log

--- python/headFirstPython/chapter_5/webapp/vsearch4web.py
+++ python/headFirstPython/chapter_5/webapp/vsearch4web.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env
 from flask import Flask, render_template, request,  escape
 from vsearch import search4letters
+from socket import gethostname
 
 app = Flask(__name__)
 
@@ -21,18 +22,22 @@ def do_search() -> 'html':
 @app.route('/') # The "entry_page" function now has two URLs associated with it.
 @app.route('/entry')
 def entry_page() -> 'html':
-    return render_template('entry.html', the_title="Welcome to search4letters on the web!")
+    host_name = gethostname()
+    return render_template('entry.html', the_title="Welcome to search4letters on the web!", the_hostname=host_name)
 
 @app.route('/viewlog')
-def view_the_log() -> str:
+def view_the_log() -> 'html':
     contents = []
     with open('vsearch.log') as log:
         for line in log:
             contents.append([])
             for item in line.split("|"):
                 contents[-1].append(escape(item))
-        
-    return str(contents)
+    titles = ('Form Data', 'Remote_addr', 'User_agent', 'Results')
+    return render_template('viewlog.html',
+                           the_title='View Log',
+                           the_row_titles=titles,
+                           the_data=contents,)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
