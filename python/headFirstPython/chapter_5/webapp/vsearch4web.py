1e31f4703713a3a5ced95a89d36f9ce1dc38500d
Thiago Cavalcante <thiagocavalcante@Thiagos-MacBook-Air.local>
2020-05-09 08:49:05 -0300

    Added dabase function

--- python/headFirstPython/chapter_5/webapp/vsearch4web.py
+++ python/headFirstPython/chapter_5/webapp/vsearch4web.py
@@ -2,12 +2,34 @@
 from flask import Flask, render_template, request,  escape
 from vsearch import search4letters
 from socket import gethostname
+import mysql.connector
 
 app = Flask(__name__)
-
+# Setup
 def log_request(req: 'flask_request', res: str) -> None:
-    with open('vsearch.log', 'a') as log:
-        print(req.form, req.remote_addr, req.user_agent, res, file=log, sep='|')
+    """Log details of the web request and the""" 
+    dbconfig = { 'host': '127.0.0.1',
+                 'user': 'vsearch',
+                 'password': 'vsearchpasswd',
+                 'database': 'vsearchlogDB', }
+    
+    conn = mysql.connector.connect(**dbconfig)
+    cursor = conn.cursor()
+# Do    
+    _SQL = """insert into log
+              (phrase, letters, ip, browser_string, results)
+              values
+              (%s, %s, %s, %s, %s)"""
+    
+    cursor.execute(_SQL, (req.form['phrase'],
+                          req.form['letters'],
+                          req.remote_addr,
+                          req.user_agent.browser,
+                          res, ))
+# Teardown
+    conn.commit()
+    cursor.close()
+    conn.close()
 
 @app.route('/search4', methods=['POST']) # GET is Default Method.
 def do_search() -> 'html':
