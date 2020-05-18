95ad54785e2da3afc6e24e5d13c75838dd2d4ceb
Thiago Cavalcante <u003688@XPMAC-10004689.local>
2020-05-18 09:06:32 -0300

    Refactoring db

--- python/headFirstPython/chapter_5/webapp/vsearch4web.py
+++ python/headFirstPython/chapter_5/webapp/vsearch4web.py
@@ -2,34 +2,27 @@
 from flask import Flask, render_template, request,  escape
 from vsearch import search4letters
 from socket import gethostname
-import mysql.connector
+from DBcm import UseDatabase
 
 app = Flask(__name__)
-# Setup
+app.config['dbconfig'] = { 'host': '127.0.0.1',
+                           'user': 'vsearch',
+                           'password': 'vsearchpasswd',
+                           'database': 'vsearchlogDB', }
+
 def log_request(req: 'flask_request', res: str) -> None:
     """Log details of the web request and the""" 
-    dbconfig = { 'host': '127.0.0.1',
-                 'user': 'vsearch',
-                 'password': 'vsearchpasswd',
-                 'database': 'vsearchlogDB', }
+    with UseDatabase(app.config['dbconfig']) as cursor:    
+        _SQL = """insert into log
+                (phrase, letters, ip, browser_string, results)
+                values
+                (%s, %s, %s, %s, %s)"""
     
-    conn = mysql.connector.connect(**dbconfig)
-    cursor = conn.cursor()
-# Do    
-    _SQL = """insert into log
-              (phrase, letters, ip, browser_string, results)
-              values
-              (%s, %s, %s, %s, %s)"""
-    
-    cursor.execute(_SQL, (req.form['phrase'],
-                          req.form['letters'],
-                          req.remote_addr,
-                          req.user_agent.browser,
-                          res, ))
-# Teardown
-    conn.commit()
-    cursor.close()
-    conn.close()
+        cursor.execute(_SQL, (req.form['phrase'],
+                            req.form['letters'],
+                            req.remote_addr,
+                            req.user_agent.browser,
+                            res, ))
 
 @app.route('/search4', methods=['POST']) # GET is Default Method.
 def do_search() -> 'html':
